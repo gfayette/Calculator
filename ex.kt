285c6adf086028f7fb2246c23a4b216827847304
G-Fayette <lancaster225@gmail.com>
2020-04-19 00:07:06 -0400

    Added some comments

--- ex.kt
+++ ex.kt
@@ -1,12 +1,9 @@
 import java.util.Scanner
 import kotlin.math.pow
 
-/**
- * Basic calculator program. Continuously asks the user for input and keeps
- * a running tally of the calculation. The user may enter 'C' to clear the
- * current calculation or 'Q' to quit the application.
- */
-class Calculator() {
+class ex() {
+
+
     private val intPattern = Regex("^[-]?[0-9]+$")
     private val floatPattern = Regex("^[-]?[0-9]+[.][0-9]*$")
     private val opPattern = Regex("^[%\\*\\+-/\\^]$")
@@ -14,14 +11,6 @@ class Calculator() {
     private var running = true
     private var clear = false
 
-    /**
-     * Parses the raw user input. Returns an Int representing the type of
-     * input and sets instance variables if the user selects the clear or
-     * quit commands.
-     *
-     * @param input The raw user input as a String.
-     * @return An integer representing the type of input
-     */
     private fun parseInput(input: String): Int {
         if (input.toUpperCase().equals("Q")) {
             running = false
@@ -41,13 +30,6 @@ class Calculator() {
         }
     }
 
-    /**
-     * Ask the user to input a number. Supports integers as well as floating
-     * point numbers. Continually asks for input until a valid number is entered
-     * or the user enters the clear or quit commands.
-     *
-     * @return A Double representing the the user's input
-     */
     private fun getNumber(): Double {
         while (true) {
             print("Enter a number:\n")
@@ -63,14 +45,6 @@ class Calculator() {
         }
     }
 
-    /**
-     * Ask the user to input an operator. Supports addition, subtraction,
-     * multiplication, division, exponentiation, and modulus. Continuously asks
-     * for input until a valid operator is entered or the user enters the clear
-     * or quit commands.
-     *
-     * @return A Char representing the operator.
-     */
     private fun getOperator(): Char {
         while (true) {
             print("Enter an operator:\n")
@@ -84,14 +58,6 @@ class Calculator() {
         }
     }
 
-    /**
-     * Performs an operation and returns the result.
-     *
-     * @param current The first number in the operation.
-     * @param operator The operation which is performed.
-     * @param number The second number in the operation.
-     * @return The result of the operation as a Double.
-     */
     private fun performOperation(current: Double, operator: Char, number: Double): Double {
         when (operator) {
             '+' -> return current + number
@@ -108,22 +74,15 @@ class Calculator() {
         }
     }
 
+    fun reset() {
+        running = true
+        clear = false
+    }
 
-    /**
-     * Runs the calculator program by continuously asking the user for numbers
-     * and operators in alternating order. Performs operations and keeps a
-     * running tally of the calculation. Checks for divide by zero errors and
-     * exits if the user enters the clear or quit command.
-     *
-     * @return The value of clear at the time the input loop exits.
-     */
     fun run(): Boolean {
         var current: Double
         var operator: Char
         var number: Double
-
-        running = true
-        clear = false
 
         current = getNumber()
         if (!running) return clear
@@ -145,18 +104,27 @@ class Calculator() {
             println("$oldCurrent $operator $number = $current")
         }
     }
+
+    fun xx(a: Long) {
+
+    }
 }
 
-/**
- * Entry point for the program. Displays a welcome message and begins the
- * program. Repeatedly calls the run() function in Calculator until the
- * user enters the quit command.
- *
- * @param args A String Array containing any arguments passed to the program.
- */
+
 fun main(args: Array<String>) {
+    val c = ex()
+
+    var a: Long = 10
+    var b: Int = 10
+    c.xx(a)
+
+
+
+
     println("Welcome! Type 'C' to clear, 'Q' to quit")
-    val c = Calculator()
-    while (c.run()) { }
+
+    while (c.run()) {
+        c.reset()
+    }
     println("Goodbye!")
 }
